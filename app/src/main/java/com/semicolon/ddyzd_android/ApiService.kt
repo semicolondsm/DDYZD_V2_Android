c9e4bab2bca77a3ede72f2030c421a4c780b51e9
minjun8946 <minjun8946@daum.net>
2021-02-07 01:21:41 +0900

    UPDATE; chat api setting

--- app/src/main/java/com/semicolon/ddyzd_android/ApiService.kt
+++ app/src/main/java/com/semicolon/ddyzd_android/ApiService.kt
@@ -1,8 +1,10 @@
 package com.semicolon.ddyzd_android
 
+import com.semicolon.ddyzd_android.model.ChatData
 import com.semicolon.ddyzd_android.model.ClubData
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.Query
 
@@ -11,4 +13,9 @@ interface ApiService {
     fun clublist(
     ): Call<ArrayList<ClubData>>
 
+
+    @GET("club/list")
+    fun chatlist(
+            @Header("access-token") assesstoken : String
+    ):Call<ArrayList<ChatData>>
 }
---
+++ app/src/main/java/com/semicolon/ddyzd_android/model/ChatData.kt
@@ -0,0 +1,6 @@
+package com.semicolon.ddyzd_android.model
+
+data class ChatData(val chatData: data)
+
+data class data(val roomid: String, val clubid: String, val clubname : String, val clubimage: String, val lastdate : String, val lastmessage : String)
+
