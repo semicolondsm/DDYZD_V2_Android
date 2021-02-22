d2221ffab466254809f43b443581ff30ce060f35
jaewonkim <turitus@naver.com>
2021-02-23 00:13:54 +0900

    UPDATE;add feed viewmodel

--- app/src/main/java/com/semicolon/ddyzd_android/ul/activity/AddFeedActivity.kt
+++ app/src/main/java/com/semicolon/ddyzd_android/ul/activity/AddFeedActivity.kt
@@ -3,10 +3,16 @@ package com.semicolon.ddyzd_android.ul.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.semicolon.ddyzd_android.R
+import com.semicolon.ddyzd_android.databinding.ActivityAddFeedBinding
+import com.semicolon.ddyzd_android.viewmodel.AddFeedViewModel
 
 class AddFeedActivity : AppCompatActivity() {
+    lateinit var binding:ActivityAddFeedBinding
+    val viewModel=AddFeedViewModel(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_feed)
+        binding= ActivityAddFeedBinding.inflate(layoutInflater)
+        binding.vm=viewModel
+        setContentView(binding.root)
     }
 }
