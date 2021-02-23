2592dc8f3eb72e04a12ac12fa47038e43b84e046
jaewonkim <turitus@naver.com>
2021-02-24 04:37:52 +0900

    UPDATE;fcm method

---
+++ app/src/main/java/com/semicolon/ddyzd_android/fcm/MsgFireBaseIdService.java
@@ -0,0 +1,21 @@
+package com.semicolon.ddyzd_android.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+public class MsgFireBaseIdService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+    }
+}
