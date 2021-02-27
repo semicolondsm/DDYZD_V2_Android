d56d1046d7be8fcf23c836b2e008ce3a7cb9d2a9
jaewonkim <turitus@naver.com>
2021-02-27 15:09:31 +0900

    UPDATE;fix error

--- app/src/main/java/com/semicolon/ddyzd_android/model/ChattingData.kt
+++ app/src/main/java/com/semicolon/ddyzd_android/model/ChattingData.kt
@@ -1,3 +1,3 @@
 package com.semicolon.ddyzd_android.model
 
-data class ChattingData(val title : String ,val msg : String, val user_type : String, val create_at : String)
+data class ChattingData(val title : String ,val msg : String, val user_type : String, val create_at : String?)
