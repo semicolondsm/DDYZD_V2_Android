d5adb67cf027622ff0604bf065f762bd39211c9b
jaewonkim <turitus@naver.com>
2021-03-03 01:23:14 +0900

    UPDATE;replace

--- app/src/main/java/com/semicolon/ddyzd_android/bindingadapter/ProfilePhotoBindingAdapter.kt
+++ app/src/main/java/com/semicolon/ddyzd_android/bindingadapter/ProfilePhotoBindingAdapter.kt
@@ -1,5 +1,6 @@
 package com.semicolon.ddyzd_android.bindingadapter
 
+import android.util.Log
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
@@ -17,10 +18,14 @@ object ProfilePhotoBindingAdapter {
     @JvmStatic
     @BindingAdapter("slideImageUrl")
     fun loadSlideImage(imageView: ImageView,url: String?){
-        if(!url.isNullOrEmpty()){
+        Log.d("사진","실행")
+        if(!url.isNullOrEmpty()) {
+            Log.d("사진", "url:$url")
             Glide.with(imageView.context)
-                .load("https://api.eungyeol.live/file/$url")
+                .load(url)
                 .error(R.drawable.image).into(imageView)
+        }else{
+            Log.d("사진","is null")
         }
     }
 
