bf74914898bda62062782aeaff7a7e25a2c71b9b
jaewonkim <turitus@naver.com>
2021-03-04 16:42:06 +0900

    DELETE;logcat

--- app/src/main/java/com/semicolon/ddyzd_android/bindingadapter/ProfilePhotoBindingAdapter.kt
+++ app/src/main/java/com/semicolon/ddyzd_android/bindingadapter/ProfilePhotoBindingAdapter.kt
@@ -18,14 +18,10 @@ object ProfilePhotoBindingAdapter {
     @JvmStatic
     @BindingAdapter("slideImageUrl")
     fun loadSlideImage(imageView: ImageView,url: String?){
-        Log.d("사진","실행")
         if(!url.isNullOrEmpty()) {
-            Log.d("사진", "url:$url")
             Glide.with(imageView.context)
                 .load("https://api.semicolon.live/file/$url")
                 .error(R.drawable.image).into(imageView)
-        }else{
-            Log.d("사진","is null")
         }
     }
 
